ab67eee8e16f82b3c6f59c95afddae350c7ea9da
Jan Noel S. Paed <jannoelpaed27@gmail.com>
2025-04-01 19:58:37 +0800

    added a navbar using react-router-dom for easy navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
